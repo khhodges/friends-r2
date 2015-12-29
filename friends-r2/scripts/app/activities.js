0e6acb8c4b3eb253baffbd2d388fda6738906f2c
Ken Hh <sipantic@gmail.com>
2015-12-29 23:27:25 +0000

    Changes made by Ken Hh (sipantic@gmail.com).

--- friends-r2/scripts/app/activities.js
+++ friends-r2/scripts/app/activities.js
@@ -5,146 +5,138 @@
 var app = app || {};
 
 app.Activities = (function () {
-    'use strict'
+	'use strict'
 
-    // Activities model
-    var activitiesModel = (function () {
+	// Activities model
+	var activitiesModel = (function () {
+		var activityModel = {
 
-        var activityModel = {
+			id: 'Id',
+			fields: {
+				Text: {
+						field: 'Text',
+						defaultValue: ''
+					},
+				CreatedAt: {
+						field: 'CreatedAt',
+						defaultValue: new Date()
+					},
+				Picture: {
+						fields: 'Picture',
+						defaultValue: null
+					},
+				UserId: {
+						field: 'UserId',
+						defaultValue: null
+					},
+				Location: {
+						field: 'Location',
+						defaultValue: null
+					},
+				Likes: {
+						field: 'Likes',
+						defaultValue: []
+					}
+			},
+			CreatedAtFormatted: function () {
+				return app.helper.formatDate(this.get('CreatedAt'));
+			},
+			PictureUrl: function () {
+				return app.helper.resolvePictureUrl(this.get('Picture'));
+			},
+			User: function () {
+				var userId = this.get('UserId');
 
-            id: 'Id',
-            fields: {
-                Text: {
-                    field: 'Text',
-                    defaultValue: ''
-                },
-                CreatedAt: {
-                    field: 'CreatedAt',
-                    defaultValue: new Date()
-                },
-                Picture: {
-                    fields: 'Picture',
-                    defaultValue: null
-                },
-                UserId: {
-                    field: 'UserId',
-                    defaultValue: null
-                },
-                Likes: {
-                    field: 'Likes',
-                    defaultValue: []
-                }
-            },
-            CreatedAtFormatted: function () {
+				var user = $.grep(app.Users.users(), function (e) {
+					return e.Id === userId;
+				})[0];
 
-                return app.helper.formatDate(this.get('CreatedAt'));
-            },
-            PictureUrl: function () {
+				return user ? {
+					DisplayName: user.DisplayName,
+					PictureUrl: app.helper.resolveProfilePictureUrl(user.Picture),
+					urlPictureUrl: app.helper.resolveBackgroundPictureUrl(user.Picture)
+				} : {
+					DisplayName: 'Anonymous',
+					PictureUrl: app.helper.resolveProfilePictureUrl()
+				};
+			},
+			isVisible: function () {
+				var currentUserId = app.Users.currentUser.data.Id;
+				var userId = this.get('UserId');
 
-                return app.helper.resolvePictureUrl(this.get('Picture'));
-            },
-            User: function () {
-
-                var userId = this.get('UserId');
-
-                var user = $.grep(app.Users.users(), function (e) {
-                    return e.Id === userId;
-                })[0];
-
-                return user ? {
-                    DisplayName: user.DisplayName,
-                    PictureUrl: app.helper.resolveProfilePictureUrl(user.Picture)
-                } : {
-                    DisplayName: 'Anonymous',
-                    PictureUrl: app.helper.resolveProfilePictureUrl()
-                };
-            },
-            isVisible: function () {
-                var currentUserId = app.Users.currentUser.data.Id;
-                var userId = this.get('UserId');
-
-                return currentUserId === userId;
-            }
-        };
-
-        // Activities data source. The Backend Services dialect of the Kendo UI DataSource component
-        // supports filtering, sorting, paging, and CRUD operations.
-        var activitiesDataSource = new kendo.data.DataSource({
-            type: 'everlive',
-            schema: {
-                model: activityModel
-            },
-            transport: {
-                // Required by Backend Services
-                typeName: 'Activities'
-            },
-            change: function (e) {
-
-                if (e.items && e.items.length > 0) {
-                    $('#no-activities-span').hide();
-                } else {
-                    $('#no-activities-span').show();
-                }
-            },
-            sort: { field: 'CreatedAt', dir: 'desc' }
-        });
-
-        return {
-            activities: activitiesDataSource
-        };
-
-    }());
-
-    // Activities view model
-    var activitiesViewModel = (function () {
-
-        // Navigate to activityView When some activity is selected
-        var activitySelected = function (e) {
-
-            app.mobileApp.navigate('views/activityView.html?uid=' + e.data.uid);
-        };
-		
-		        // Navigate to activityView When some activity is selected
-/*        var activityDefault = function (e) {
-						        
-			var listView = $("#activities-listview").data("kendoMobileListView");
-			var children = listView.element.children();
-			var activityUid;
-			for (var x = 0; x < children.length; x++) {
-				if (listView.element.children()[x].id === id) {
-					activityUid = listView.element.children()[x].Uid;
-				}
+				return currentUserId === userId;
 			}
+		};
 
-            app.mobileApp.navigate('views/addActivityView.html?uid=' + e.data.uid);
-        };*/
+		// Activities data source. The Backend Services dialect of the Kendo UI DataSource component
+		// supports filtering, sorting, paging, and CRUD operations.
+		var activitiesDataSource = new kendo.data.DataSource({
+																 type: 'everlive',
+																 schema: {
+				model: activityModel
+			},
+																 transport: {
+				// Required by Backend Services
+				typeName: 'Activities'
+			},
+																 change: function (e) {
+																	 if (e.items && e.items.length > 0) {
+																		 $('#no-activities-span').hide();
+																	 } else {
+																		 $('#no-activities-span').show();
+																	 }
+																 },
+																 sort: { field: 'CreatedAt', dir: 'desc' }
+															 });
 
-        // Navigate to app home
-        var navigateHome = function () {
+		return {
+			activities: activitiesDataSource
+		};
+	}());
 
-            app.mobileApp.navigate('#welcome');
-        };
-
-        // Logout user
-        var logout = function () {
-
-            app.helper.logout()
-            .then(navigateHome, function (err) {
-                app.showError(err.message);
-                navigateHome();
-            });
-        };
+	// Activities view model
+	var activitiesViewModel = (function () {
+		// Navigate to activityView When some activity is selected
+		var activitySelected = function (e) {
+			app.mobileApp.navigate('views/activityView.html?uid=' + e.data.uid);
+		};
 		
+		// Navigate to activityView When some activity is selected
+		/*        var activityDefault = function (e) {
+						        
+		var listView = $("#activities-listview").data("kendoMobileListView");
+		var children = listView.element.children();
+		var activityUid;
+		for (var x = 0; x < children.length; x++) {
+		if (listView.element.children()[x].id === id) {
+		activityUid = listView.element.children()[x].Uid;
+		}
+		}
 
-        return {
-            activities: activitiesModel.activities,
-            activitySelected: activitySelected,
+		app.mobileApp.navigate('views/addActivityView.html?uid=' + e.data.uid);
+		};*/
+
+		// Navigate to app home
+		var navigateHome = function () {
+			app.mobileApp.navigate('#welcome');
+		};
+
+		// Logout user
+		var logout = function () {
+			app.helper.logout()
+				.then(navigateHome, function (err) {
+					app.showError(err.message);
+					navigateHome();
+				});
+		};
+
+		return {
+			activities: activitiesModel.activities,
+			activitySelected: activitySelected,
 			//activityDefault: activityDefault,
-            logout: logout
-        };
+			logout: logout
+		};
+	}());
 
-    }());
-
-    return activitiesViewModel;
-
+	return activitiesViewModel;
 }());
